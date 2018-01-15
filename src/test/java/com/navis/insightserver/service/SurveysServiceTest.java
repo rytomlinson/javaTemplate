6bee1e0ab45b7a9fefe67da91f7202f94bf588ad
Romina Tornello <rominatornello.gotocme@gmail.com>
2018-01-15 16:14:46 -0300

    RCH-693: Test SurveyService - GetSurveys,UpdateSurveyPublishStatus

--- src/test/java/com/navis/insightserver/service/SurveysServiceTest.java
+++ src/test/java/com/navis/insightserver/service/SurveysServiceTest.java
@@ -14,10 +14,14 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 public class SurveysServiceTest {
+
+    List<SurveyEntity> surveyEntityList = new ArrayList<>();
 
     @Mock
     SurveyRepository surveyRepository;
@@ -30,10 +34,10 @@ public class SurveysServiceTest {
 
     @Before
     public void setUp() {
-        //Mock a Survey Entity List
+        //Mock a Survey Entity
         surveyEntity = new SurveyEntity();
         surveyEntity.setId(2L);
-        surveyEntity.setEnabled(true);
+        surveyEntity.setEnabled(false);
         surveyEntity.setCreateDate(new Date());
         I18NStringEntity i18NStringEntityTitle = new I18NStringEntity();
         i18NStringEntityTitle.setId(3L);
@@ -42,14 +46,47 @@ public class SurveysServiceTest {
         i18NStringEntityTitle.setId(34L);
         surveyEntity.setI18NStringByDescriptionId(i18NStringEntityDescription);
         Mockito.when(surveyRepository.findByOwnerAndId(null, null)).thenReturn(surveyEntity);
+
+        //Mock a Survey List
+        surveyEntityList.add(surveyEntity);
+        Mockito.when(surveyRepository.findByOwner(null)).thenReturn(surveyEntityList);
+        Mockito.when(surveyRepository.findByOwnerAndDeletedFalse(null)).thenReturn(surveyEntityList);
+
+        //Mock Exist and Find Survey
+        Mockito.when(surveyRepository.exists(null)).thenReturn(true);
+        Mockito.when(surveyRepository.findOne(null)).thenReturn(surveyEntity);
+        Mockito.when(surveyRepository.getCurrentSurveyQuestionCount(null)).thenReturn(1L);
+
     }
 
     @Test
     public void testGetSurveyById() {
-        SurveyDTO surveyEntity = surveysService.getSurveyById(null, null, null);
-        Assert.assertNotNull(surveyEntity);
-
+        SurveyDTO surveyEnt = surveysService.getSurveyById(null, null, null);
+        Assert.assertNotNull(surveyEnt);
+        Assert.assertEquals(surveyEnt.getEnabled(), false);
     }
 
+
+    @Test
+    public void testGetSurveysIncludeDeleted() {
+        List<SurveyDTO> surveyEntityList = surveysService.getSurveys(null, null, true);
+        Assert.assertNotNull(surveyEntityList);
+        Assert.assertEquals(surveyEntityList.size() > 0, true);
+    }
+
+    @Test
+    public void testGetSurveysNotIncludeDeleted() {
+        List<SurveyDTO> surveyEntityList = surveysService.getSurveys(null, null, true);
+        Assert.assertNotNull(surveyEntityList);
+        Assert.assertEquals(surveyEntityList.size() > 0, true);
+    }
+
+    @Test
+    public void testUpdateSurveyPublishStatus_PUBLISH() {
+        surveysService.updateSurveyPublishStatus(null, null, true);
+        Assert.assertNotNull(surveyEntity);
+        Assert.assertEquals(surveyEntity.getEnabled(), true);
+
+    }
 
 }
